bcbf8c9a1149b5956454eac57c7944d99f298557
TheRealKeto <therealketo@gmail.com>
2021-07-08 21:29:46 -0400

    Small changes and fixes
    - Adapt to new Makefile path
    - Add homepage field to control file
    - Improve control file description field

--- makefiles/abduco.mk
+++ makefiles/abduco.mk
@@ -3,35 +3,35 @@ $(error Use the main Makefile)
 endif
 
 SUBPROJECTS    += abduco
-ABDUCO_VERSION := 0.6
+ABDUCO_COMMIT  := fcac3b17edcb7cd7c22305bd5c519f9cd7e6c1de
+ABDUCO_VERSION := 0.6.$(shell echo $(ABDUCO_COMMIT) | cut -c -7)
 DEB_ABDUCO_V   ?= $(ABDUCO_VERSION)
 
 abduco-setup: setup
-	$(call GITHUB_ARCHIVE,martanne,abduco,$(ABDUCO_VERSION),master)
-	$(call EXTRACT_TAR,abduco-$(ABDUCO_VERSION).tar.gz,abduco-master,abduco)
+	# Download commit that actually builds (maybe?)
+	# Change this until (possible) next release tag
+	$(call GITHUB_ARCHIVE,martanne,abduco,$(ABDUCO_COMMIT),$(ABDUCO_COMMIT))
+	$(call EXTRACT_TAR,abduco-$(ABDUCO_COMMIT).tar.gz,abduco-$(ABDUCO_COMMIT),abduco)
 
 ifneq ($(wildcard $(BUILD_WORK)/abduco/.build_complete),)
 abduco:
 	@echo "Using previously built abduco."
 else
 abduco: abduco-setup
-	# Patch configure script options
-	$(SED) -i "211,212d" $(BUILD_WORK)/abduco/configure
 	cd $(BUILD_WORK)/abduco && ./configure \
 		$(DEFAULT_CONFIGURE_FLAGS)
 	+$(MAKE) -C $(BUILD_WORK)/abduco
 	+$(MAKE) -C $(BUILD_WORK)/abduco install \
-		CC=$(CC) \
-		CFLAGS=$(CFLAGS) \
-		LDFLAGS=$(LDFLAGS) \
-		PREFIX=$(MEMO_PREFIX)$(MEMO_SUB_PREFIX) \
-		DESTDIR=$(BUILD_STAGE)/abduco
+		CC="$(CC)" \
+		CFLAGS="$(CFLAGS)" \
+		LDFLAGS="$(LDFLAGS)" \
+		PREFIX="$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)" \
+		DESTDIR="$(BUILD_STAGE)/abduco"
 	touch $(BUILD_WORK)/abduco/.build_complete
 endif
 
 abduco-package: abduco-stage
 	# abduco.mk Package Structure
-	mkdir -p $(BUILD_DIST)/abduco
 	cp -a $(BUILD_STAGE)/abduco $(BUILD_DIST)
 
 	# abduco.mk Sign
