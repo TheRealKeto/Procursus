5c28886ebc8eb2020f2f5c79f74ad3382a53d482
Keto <therealketo@gmail.com>
2023-08-26 19:08:00 -0400

    pinentry: Add initial support for pinentry-mac

--- makefiles/pinentry.mk
+++ makefiles/pinentry.mk
@@ -3,25 +3,26 @@ $(error Use the main Makefile)
 endif
 
 SUBPROJECTS      += pinentry
-PINENTRY_VERSION := 1.2.0
+PINENTRY_VERSION := 1.1.1.1
 DEB_PINENTRY_V   ?= $(PINENTRY_VERSION)
 
 pinentry-setup: setup
-	$(call DOWNLOAD_FILES,$(BUILD_SOURCE),https://gnupg.org/ftp/gcrypt/pinentry/pinentry-$(PINENTRY_VERSION).tar.bz2{$(comma).sig})
-	$(call PGP_VERIFY,pinentry-$(PINENTRY_VERSION).tar.bz2)
-	$(call EXTRACT_TAR,pinentry-$(PINENTRY_VERSION).tar.bz2,pinentry-$(PINENTRY_VERSION),pinentry)
+	$(call GITHUB_ARCHIVE,GPGTools,pinentry,$(PINENTRY_VERSION),v$(PINENTRY_VERSION))
+	$(call EXTRACT_TAR,pinentry-$(PINENTRY_VERSION).tar.gz,pinentry-$(PINENTRY_VERSION),pinentry)
 
 ifneq ($(wildcard $(BUILD_WORK)/pinentry/.build_complete),)
 pinentry:
 	@echo "Using previously built pinentry."
 else
 pinentry: pinentry-setup libgpg-error libassuan ncurses
+	cd $(BUILD_WORK)/pinentry && autoreconf -fiv
 	cd $(BUILD_WORK)/pinentry && ./configure -C \
 		$(DEFAULT_CONFIGURE_FLAGS) \
 		--with-libassuan-prefix=$(BUILD_BASE)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX) \
 		--with-libgpg-error-prefix=$(BUILD_BASE)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX) \
 		--enable-pinentry-tty \
 		--enable-pinentry-ncurses \
+		--enable-maintainer-mode \
 		NCURSES_CFLAGS="-DNCURSES_WIDECHAR"
 	+$(MAKE) -C $(BUILD_WORK)/pinentry
 	+$(MAKE) -C $(BUILD_WORK)/pinentry install \
@@ -46,4 +47,3 @@ pinentry-package: pinentry-stage
 	rm -rf $(BUILD_DIST)/pinentry
 
 .PHONY: pinentry pinentry-package
-
